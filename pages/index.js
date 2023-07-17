430f90c7477ff543b656c9f742aed84fcd8b4b9c
Ronaldo Lavestein <71901765+Lavestein@users.noreply.github.com>
2023-07-17 17:56:57 +0000

    mensagem do commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Primeiro projeto:</h1>
+      <h4>Página criada usando Node.JS, Next.JS e React</h4>
+    </div>
+  );
+}
+
+export default Home;
