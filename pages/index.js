becfdac40f029cddae490c645153ef3a49f69a3d
Ronaldo Lavestein <71901765+Lavestein@users.noreply.github.com>
2023-07-18 13:42:27 +0000

    Alterações com ajuda do Chat GPT

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Link from "next/link";
 
 function Home() {
+  const buttonRef = useRef(null);
+
   const handleDownloadPDF = () => {
     const link = document.createElement("a");
     link.href = "/PCII.pdf";
@@ -13,15 +15,128 @@ function Home() {
     window.open("/PCII.pdf", "_blank");
   };
 
+  useEffect(() => {
+    const buttons = document.querySelectorAll("button");
+    let maxWidth = 0;
+
+    buttons.forEach((button) => {
+      const { offsetWidth } = button;
+      if (offsetWidth > maxWidth) {
+        maxWidth = offsetWidth;
+      }
+    });
+
+    buttons.forEach((button) => {
+      button.style.width = `${maxWidth}px`;
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>Primeiro projeto:</h1>
-      <h4>Página criada usando Node.JS, Next.JS e React</h4>
-      <button onClick={handleDownloadPDF}>Baixar PDF</button>
-      <button onClick={handleOpenPDF}>Abrir PDF</button>
-      <p>
-        <Link href="/sobre">Sobre</Link>
+    <div
+      style={{
+        backgroundColor: "#ffffff",
+        padding: "40px",
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          color: "#333333",
+          fontSize: "28px",
+          fontWeight: "bold",
+          marginBottom: "20px",
+          textTransform: "uppercase",
+        }}
+      >
+        Professor Ronaldo Lavestein
+      </h1>
+      <p style={{ color: "#333333", fontSize: "18px", marginBottom: "20px" }}>
+        ETEC Dr. Francisco Nogueira de Lima
       </p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <button
+          ref={buttonRef}
+          style={{
+            backgroundColor: "#3498db",
+            color: "#ffffff",
+            border: "1px solid #3498db",
+            borderRadius: "4px",
+            padding: "10px 30px",
+            cursor: "pointer",
+            marginRight: "10px",
+            transition: "background-color 0.3s ease",
+            width: "120px",
+            whiteSpace: "nowrap",
+          }}
+          onClick={handleDownloadPDF}
+          onMouseEnter={(e) => {
+            e.target.style.backgroundColor = "#ffffff";
+            e.target.style.color = "#3498db";
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.backgroundColor = "#3498db";
+            e.target.style.color = "#ffffff";
+          }}
+        >
+          Baixar PDF
+        </button>
+        <button
+          style={{
+            backgroundColor: "#3498db",
+            color: "#ffffff",
+            border: "1px solid #3498db",
+            borderRadius: "4px",
+            padding: "10px 30px",
+            cursor: "pointer",
+            transition: "background-color 0.3s ease",
+            width: "120px",
+            whiteSpace: "nowrap",
+          }}
+          onClick={handleOpenPDF}
+          onMouseEnter={(e) => {
+            e.target.style.backgroundColor = "#ffffff";
+            e.target.style.color = "#3498db";
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.backgroundColor = "#3498db";
+            e.target.style.color = "#ffffff";
+          }}
+        >
+          Abrir PDF
+        </button>
+        <Link href="/sobre" passHref>
+          <button
+            style={{
+              backgroundColor: "#3498db",
+              color: "#ffffff",
+              border: "1px solid #3498db",
+              borderRadius: "4px",
+              padding: "10px 30px",
+              cursor: "pointer",
+              marginLeft: "10px",
+              transition: "background-color 0.3s ease",
+              width: "120px",
+              whiteSpace: "nowrap",
+            }}
+            onMouseEnter={(e) => {
+              e.target.style.backgroundColor = "#ffffff";
+              e.target.style.color = "#3498db";
+            }}
+            onMouseLeave={(e) => {
+              e.target.style.backgroundColor = "#3498db";
+              e.target.style.color = "#ffffff";
+            }}
+          >
+            Sobre
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,12 +1,48 @@
+import React from "react";
 import Link from "next/link";
 
 function Sobre() {
   return (
-    <div>
-      <h1>Programação de Computadores II </h1>
-      <h4>Curso Técnico em Informática </h4>
-      <h5>Professor Ronaldo Lavestein - Escola Industrial - Casa Branca</h5>
-      <Link href="/">Voltar</Link>
+    <div
+      style={{
+        backgroundColor: "#ffffff",
+        padding: "40px",
+        textAlign: "center",
+        fontFamily: "Verdana, sans-serif",
+      }}
+    >
+      <p
+        style={{
+          color: "#333333",
+          fontSize: "28px",
+          marginBottom: "20px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        }}
+      >
+        Programação de Computadores II
+      </p>
+      <p style={{ color: "#333333", fontSize: "20px", marginBottom: "20px" }}>
+        Curso Técnico em Informática
+      </p>
+      <p style={{ color: "#333333", fontSize: "18px", marginBottom: "20px" }}>
+        Professor Ronaldo Lavestein - Escola Industrial - Casa Branca
+      </p>
+      <Link href="/">
+        <button
+          style={{
+            backgroundColor: "#3498db",
+            color: "#ffffff",
+            border: "1px solid #3498db",
+            borderRadius: "4px",
+            padding: "10px 20px",
+            cursor: "pointer",
+            transition: "background-color 0.3s ease",
+          }}
+        >
+          Voltar
+        </button>
+      </Link>
     </div>
   );
 }
